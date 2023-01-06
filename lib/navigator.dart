c12bb21756f74901d3340fcd89e4dc311ec3b71a
Shubham Joshi <joshishubham0301@gmail.com>
2023-01-06 16:32:28 +0530

    🧭Bottom Navigation bar added

--- lib/navigator.dart
+++ lib/navigator.dart
@@ -23,6 +23,14 @@ class _NavigationHandlerState extends State<NavigationHandler> {
       'Index 2: Service',
       style: optionStyle,
     ),
+    Text(
+      'Index 3: Complaint',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: More',
+      style: optionStyle,
+    ),
   ];
 
   void _onItemTapped(int index) {
@@ -38,7 +46,11 @@ class _NavigationHandlerState extends State<NavigationHandler> {
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        selectedIconTheme: const IconThemeData(color: Color(0xff540375),),
+        selectedIconTheme: const IconThemeData(
+          color: Color(0xff540375),
+        ),
+        unselectedItemColor: const Color(0xff8A8A8A),
+        showUnselectedLabels: true,
         selectedLabelStyle: Fonts.inter_400
             .copyWith(fontSize: 12, color: const Color(0xff540375)),
         items: const <BottomNavigationBarItem>[
@@ -47,12 +59,20 @@ class _NavigationHandlerState extends State<NavigationHandler> {
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
+            icon: Icon(Icons.food_bank),
             label: 'Food',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
+            icon: Icon(Icons.room_service),
             label: 'Service',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.room_service),
+            label: 'Complaint',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more),
+            label: 'More',
           ),
         ],
         currentIndex: _selectedIndex,
